647f83c3e928057dc7fea307d098cf055f5c989b
Rohin Bhushan <rohinbhushan@gmail.com>
2015-01-30 17:17:47 -0800

    Arm/elevator positions and movement added
    Arms should now open/close and rise/lower with button input

--- src/org/usfirst/frc3482/robot/commands/LowerArms.java
+++ src/org/usfirst/frc3482/robot/commands/LowerArms.java
@@ -34,6 +34,7 @@ public class  LowerArms extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.arms.stepDown();
     }
 
     // Make this return true when this Command no longer needs to run execute()
